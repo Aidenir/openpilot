95b6f1b16936dd452882f28284fc27afedb7e35a
Aidenir <viktor.hansson@me.com>
2026-01-02 16:52:22 +0100

    Add driver awareness metric to developer sidebar
    Added configurable "Driver Monitoring: Awareness" metric showing awareness
    status as percentage (100% = fully attentive, 0% = terminal).

--- frogpilot/ui/qt/widgets/developer_sidebar.cc
+++ frogpilot/ui/qt/widgets/developer_sidebar.cc
@@ -66,6 +66,7 @@ void DeveloperSidebar::updateState(const UIState &s, const FrogPilotUIState &fs)
 
   const cereal::CarControl::Reader &carControl = fpsm["carControl"].getCarControl();
   const cereal::CarState::Reader &carState = fpsm["carState"].getCarState();
+  const cereal::DriverMonitoringState::Reader &driverMonitoring = fpsm["driverMonitoringState"].getDriverMonitoringState();
   const cereal::FrogPilotPlan::Reader &frogpilotPlan = fpsm["frogpilotPlan"].getFrogpilotPlan();
   const cereal::LiveDelayData::Reader &liveDelay = fpsm["liveDelay"].getLiveDelay();
   const cereal::LiveParametersData::Reader &liveParameters = fpsm["liveParameters"].getLiveParameters();
@@ -117,6 +118,7 @@ void DeveloperSidebar::updateState(const UIState &s, const FrogPilotUIState &fs)
   accelerationStatus = ItemStatus(QPair<QString, QString>(tr("ACCEL"), QString::number(acceleration, 'f', 2) + accelerationUnit), metricColor);
   accelerationJerkStatus = ItemStatus(QPair<QString, QString>(tr("ACCEL JERK"), QString::number(frogpilotPlan.getAccelerationJerk())), metricColor);
   actuatorAccelerationStatus = ItemStatus(QPair<QString, QString>(tr("ACT ACCEL"), QString::number(carControl.getActuators().getAccel() * accelerationConversion, 'f', 2) + accelerationUnit), metricColor);
+  awarenessStatus = ItemStatus(QPair<QString, QString>(tr("AWARENESS"), QString::number(driverMonitoring.getAwarenessStatus() * 100.0f, 'f', 1) + "%"), metricColor);
   dangerFactorStatus = ItemStatus(QPair<QString, QString>(tr("DANGER %"), QString::number(frogpilotPlan.getDangerFactor() * 100.0f, 'f', 2) + "%"), metricColor);
   dangerJerkStatus = ItemStatus(QPair<QString, QString>(tr("DANGER JERK"), QString::number(frogpilotPlan.getDangerJerk())), metricColor);
   delayStatus = ItemStatus(QPair<QString, QString>(tr("STEER DELAY"), QString::number(liveDelay.getLateralDelay(), 'f', 5)), metricColor);
@@ -158,6 +160,7 @@ void DeveloperSidebar::paintEvent(QPaintEvent *event) {
   metricMap.insert(14, &accelerationJerkStatus);
   metricMap.insert(15, &dangerJerkStatus);
   metricMap.insert(16, &speedJerkStatus);
+  metricMap.insert(17, &awarenessStatus);
 
   int count = 0;
   for (size_t i = 0; i < metricAssignments.size(); ++i) {
