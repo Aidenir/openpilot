e66886890bfe4206c831d5258e8e80641fb7fe16
Aidenir <viktor.hansson@me.com>
2026-01-05 15:28:30 +0100

    Register MassageReminderTimeLeft param key

--- common/params.cc
+++ common/params.cc
@@ -411,6 +411,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"MapSpeedLimit", CLEAR_ON_MANAGER_START},
     {"MapStyle", PERSISTENT},
     {"MassageReminder", PERSISTENT},
+    {"MassageReminderTimeLeft", CLEAR_ON_MANAGER_START},
     {"MaxDesiredAcceleration", PERSISTENT},
     {"MinimumBackupSize", PERSISTENT},
     {"MinimumLaneChangeSpeed", PERSISTENT},
